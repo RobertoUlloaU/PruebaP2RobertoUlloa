62fa8f850dc7dd9b3e34a79609af0dbec07efb64
UlloaRoberto <roberto.ulloa@udla.edu.ec>
2025-06-03 07:16:33 -0500

    Add project files.

---
+++ PruebaP2RobertoUlloa/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace PruebaP2RobertoUlloa
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ PruebaP2RobertoUlloa/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PruebaP2RobertoUlloa
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
