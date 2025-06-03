9c04e5e862322c3cd45be7fcb3e5a3b9e48a0a4b
UlloaRoberto <roberto.ulloa@udla.edu.ec>
2025-06-03 08:22:59 -0500

    ya se corrigio el erro tuve que eliminarlo y volver a crear el gridlayout

--- PruebaP2RobertoUlloa/GridLayout.xaml.cs
+++ PruebaP2RobertoUlloa/GridLayout.xaml.cs
@@ -2,15 +2,12 @@ namespace PruebaP2RobertoUlloa;
 
 public partial class GridLayout : ContentPage
 {
-    public GridLayout()
-    {
-        InitializeComponent();
-
-    }
-
+	public GridLayout()
+	{
+		InitializeComponent();
+	}
     private async void OnBackButtonClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync("///ChistePage");
     }
-
 }
